0ae7c76abc0d64a23620282e4c7ec4c6fd656f4e
Davante <robinsondavante10@gmail.com>
2023-08-23 10:53:25 -0400

    added code to parse the JSON

--- YoutubeRebuild/Model.swift
+++ YoutubeRebuild/Model.swift
@@ -31,10 +31,21 @@ class Model {
                 return
             }
           
-            // parsing the data into video objects
-            
-            
-            
+            do {
+                
+                // parsing the data into video objects
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                
+               let response = try decoder.decode(Response.self, from: data!)
+                
+                dump(response)
+            }
+            catch {
+                
+                
+            }
+           
         }
         
         // kick off the task
--- YoutubeRebuild/constants.swift
+++ YoutubeRebuild/constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Constants {
     
-    static var API_KEY = "AIzaSyADBt-dDa246mM-MO80_yoffNHfYteeD-s"
+    static var API_KEY = ""
     static var PLAYLIST_ID = "PLMRqhzcHGw1Y5Cluhf7pKRNZtKaA3Q4kg"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
